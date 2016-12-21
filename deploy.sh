0636ba1a838ff81b366c44daa38a32e7c72fb4c8
Martino Facchin <m.facchin@arduino.cc>
2016-12-21 17:05:27 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -xe
+GIT_REV=`git log --pretty=format:'%h' -n 1`
+BUILD_DATE=`date +%Y-%m-%d:%H:%M:%S`
+COMPILEINFO="$GIT_REV+$BUILD_DATE"
+
+VERSION=`cat main.go| grep "const AppVersion" |cut -f4 -d " " | tr -d '"'`
+
+#Remember to set GOROOT accordingly with your installation
+
+export GOPATH=$PWD
+
+rm -rf distrib/
+
+declare -a target_folders=("linux64" "linux32" "linuxarm" "osx" "windows")
+
+mkdir distrib
+
+for folder in "${target_folders[@]}"
+do
+   mkdir -p distrib/$folder/bin/
+   go build -o distrib/$folder/bin/arduinoOTA -ldflags "-X main.compileInfo=\"$COMPILEINFO\"" main.go
+
+done
+
+#Fix windows binary extension
+mv distrib/windows/bin/arduinoOTA distrib/windows/bin/arduinoOTA.exe
+
+cd distrib
+
+for folder in "${target_folders[@]}"
+do
+   cd $folder
+   if [[ $folder == "windows" ]]; then
+	zip -r ../arduinoOTA-$VERSION-windows.zip bin/
+   else
+	tar cjf ../arduinoOTA-$VERSION-$folder.tar.bz2 bin/
+   fi
+   cd ..
+done
+
+echo =======
+ls -la arduinoOTA*
+echo =======
+sha256sum arduinoOTA*
+echo =======
+shasum arduinoOTA*
+
