b63ef47d904222338eff1d5b6837d6f144749848
per1234 <accounts@perglass.com>
2021-10-01 19:39:22 -0700

    Protect variables from globbing and word splitting in deploy script
    Stinginess with quoting in shell scripts is the most common cause of confusing bugs that usually only occur under
    specific uncommon conditions. For this reason, all variables must be quoted.
    
    More information:
    https://github.com/koalaman/shellcheck/wiki/SC2086

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 GIT_REV="$(git log --pretty=format:'%h' -n 1)"
 BUILD_DATE="$(date +%Y-%m-%d:%H:%M:%S)"
-COMPILEINFO="$(echo +$GIT_REV+$BUILD_DATE | tr -d '"')"
+COMPILEINFO="$(echo "+${GIT_REV}+${BUILD_DATE}" | tr -d '"')"
 
 VERSION="$(cat main.go| grep "const AppVersion" |cut -f4 -d " " | tr -d '"')"
 
@@ -19,9 +19,9 @@ mkdir distrib
 for folder in "${target_folders[@]}"
 do
 
-  IFS=_ read -a fields <<< $folder
-  mkdir -p distrib/$folder/bin/
-  GOOS=${fields[0]} GOARCH=${fields[1]} go build -o distrib/$folder/bin/arduinoOTA -ldflags "-X main.compileInfo=$COMPILEINFO" main.go
+  IFS=_ read -a fields <<< "$folder"
+  mkdir -p "distrib/${folder}/bin/"
+  GOOS="${fields[0]}" GOARCH="${fields[1]}" go build -o "distrib/${folder}/bin/arduinoOTA" -ldflags "-X main.compileInfo=$COMPILEINFO" main.go
 done
 
 #Fix windows binary extension
@@ -31,11 +31,11 @@ cd distrib
 
 for folder in "${target_folders[@]}"
 do
-  mv $folder arduinoOTA
+  mv "$folder" arduinoOTA
   if [[ $folder == "windows_386" ]]; then
-    zip -r arduinoOTA-$VERSION-$folder.zip arduinoOTA/
+    zip -r "arduinoOTA-${VERSION}-${folder}.zip" arduinoOTA/
   else
-    tar cjf arduinoOTA-$VERSION-$folder.tar.bz2 arduinoOTA/
+    tar cjf "arduinoOTA-${VERSION}-${folder}.tar.bz2" arduinoOTA/
   fi
   rm -rf arduinoOTA
 done
